fdeeaf4d6d831cef0198d991711148832aa4c7b0
canerozturk06 <canerata85@gmail.com>
2025-01-24 22:08:31 +0300

    caner/24.01/auth

--- src/test/java/utilities/API_Utilities/Authentication.java
+++ src/test/java/utilities/API_Utilities/Authentication.java
@@ -36,6 +36,13 @@ public class Authentication{
                 reqBody.put("email", ConfigReader.getProperty("studentEmail","api"));
                 reqBody.put("password", ConfigReader.getProperty("studentPassword","api"));
                 break;
+            case "adminCaner":
+                spec.pathParams("pp1", "api", "pp2", "getToken");
+                reqBody = new JSONObject();
+                reqBody.put("email", ConfigReader.getProperty("adminCanerEmail","api"));
+                reqBody.put("password", ConfigReader.getProperty("adminPassword","api"));
+                break;
+
         }
 
         Response response = given()
