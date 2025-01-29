2890a706906a35772e96003c22f77472b472e8ab
canerozturk06 <canerata85@gmail.com>
2025-01-29 20:08:45 +0300

    caner

--- src/test/java/stepdefinitions/API_Stepdefinitions.java
+++ src/test/java/stepdefinitions/API_Stepdefinitions.java
@@ -8,6 +8,7 @@ import io.cucumber.java.en.Given;
 
 import io.cucumber.java.en.Then;
 
+import io.cucumber.java.en.When;
 import io.restassured.http.ContentType;
 import io.restassured.path.json.JsonPath;
 import io.restassured.response.Response;
